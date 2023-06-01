f69f79193296c8b08b779d7ccecd731cf8b0c05a
Andrey Lechev <andrey@lechev.me>
2023-06-02 00:43:21 +0200

    Remove API Keys from the repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase_options_old.dart';
 
 void main() {
   runApp(const MyApp());
